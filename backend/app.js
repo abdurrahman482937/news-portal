f518cedf81719838ee1328f99d06132b56f8c0e7
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-14 21:41:26 +0600

    app

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config();
+const app = express()
+const port = 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xesam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send("News Portal Server Running...")
+})
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db("news-portal")
+        const newsCollection = db.collection("allNews")
+
+        app.get("/all-news", async (req, res) => {
+            const query = {}
+            const news = await newsCollection.find(query).toArray()
+            res.send(news)
+        })
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log("http://localhost:5000");
+})
